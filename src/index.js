7b2e0133b77331b64abdbbfa3df073607e53a7e7
YevhenTsilenko <fiboss1992@gmail.com>
2021-07-19 11:35:45 +0300

    hw13
    сделал функционал

--- src/index.js
+++ src/index.js
@@ -1,54 +1,66 @@
 import './sass/main.scss';
 import fetchImg from './js/apiService';
+import { refreshPageCounter } from './js/apiService';
 import imageTemp from './templates/imageTemp.hbs';
+import '@pnotify/core/dist/BrightTheme.css';
+import '@pnotify/core/dist/PNotify.css';
+const { error } = require('@pnotify/core');
 
 const refs = {
     form : document.querySelector('#search-form'),
     imageList : document.querySelector('.gallery'),
+    button : document.querySelector('[data-action="show-more"]'),
 }
 
 
-   
 refs.form.addEventListener('submit', onSearch);
+refs.button.addEventListener('click', showMore);
 
-async function onSearch (event) {
+function onSearch (event) {
     event.preventDefault();
+    clearImageList();
+    refreshPageCounter();
+    createImages();
+}
+
+
+function renderImages (images) {
+    refs.imageList.insertAdjacentHTML('beforeend', imageTemp(images));
+}
+
+async function createImages() {
     const text = refs.form.elements.query.value.trim();
     if(!text) {
         return ;
     }
 
     try {
-        const images = await fetchImg(text);
+        const images = await fetchImg();
+        if(images.length === 0) {
+            error({
+                title: 'Error',
+                text: 'try another word',
+              });
+        }
         renderImages(images);
+        btnScrollElem();
         
     } catch (error){
         console.log(error);
     }
-
 }
 
-function renderImages (images) {
-    refs.imageList.insertAdjacentHTML('beforeend', imageTemp(images));
+async function showMore() {
+    createImages();
 }
 
-async function onScroll ([entrie]) {
-     if(!entrie.isIntersecting) {
-        return;
-    }
-    
-    try {
-        const images = await fetchImg(text);
-        renderImages(images);
-        
-    } catch (error){
-        console.log(error);
-    }
-    
-}
+function clearImageList() {
+    refs.imageList.innerHTML='';
+  }
 
-const observer = new IntersectionObserver (onScroll, {
-    threshold: 0,
-});
-
-observer.observe(refs.imageList);
+function btnScrollElem() {
+    refs.button.scrollIntoView({
+      behavior: 'smooth',
+      block: 'end',
+    });
+  }
--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,18 +1,23 @@
 const URL = 'https://pixabay.com/api/';
 const KEY = '22485348-da19140f48b52c54df68447da';
 
-export default  async function fetchImg (text) {
-
-    let page = 1;
-
+let page = 1;
+export default  async function fetchImg () {
+    const text = document.querySelector('#search-form').elements.query.value.trim();
+    
     const response = await fetch(`${URL}?image_type=photo&orientation=horizontal&q=${text}&page=${page}&per_page=12&key=${KEY}`);
     const images = await response.json();
-
     pageIncrement ();
-
+   
     return await images.hits;
 
-    function pageIncrement () {
-        page += 1;
-    }
+
+}
+
+function pageIncrement () {
+  page += 1;
+}
+
+export function refreshPageCounter() {
+  page = 1;
 }
